0e6abb3b265881fc5ad5c731119b2de79f41e99a
shelbypalmer4 <Palmer904@live.missouristate.edu>
2023-04-24 12:15:21 -0500

    Create allele_freq_by_locality.R

---
+++ allele_freq_by_locality.R
@@ -0,0 +1,15 @@
+#### Generate allele frequency by sampling locality ####
+
+setwd("C:/Users/Shelby Palmer/Desktop/CHICKADEES")
+PoecileAlleles <- read.csv("PCR-RFLP_data_genetics_24apr2023.csv")
+
+PoecileAlleleFreq <- data.frame(Locality=unique(PoecileAlleles$Pop))
+
+for(i in 1:length(PoecileAlleleFreq$Locality)) {
+  PoecileAlleleFreq$BC_allele[i] <- length(which(PoecileAlleles$Pop==PoecileAlleleFreq$Locality[i] & PoecileAlleles[]==1))
+  PoecileAlleleFreq$CA_allele[i] <- length(which(PoecileAlleles$Pop==PoecileAlleleFreq$Locality[i] & PoecileAlleles[]==2))
+  PoecileAlleleFreq$BC_allele_freq[i] <- PoecileAlleleFreq$BC_allele[i]/sum(PoecileAlleleFreq$BC_allele[i], PoecileAlleleFreq$CA_allele[i])
+  PoecileAlleleFreq$CA_allele_freq[i] <- PoecileAlleleFreq$CA_allele[i]/sum(PoecileAlleleFreq$BC_allele[i], PoecileAlleleFreq$CA_allele[i])
+}
+
+
